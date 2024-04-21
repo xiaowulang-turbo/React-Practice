5abb2cfe5547fbdb20721c5299c9489bb39b6010
xiaowulang-turbo <2728795232@qq.com>
2024-04-21 14:49:12 +0800

    feat: finish

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -78,16 +78,20 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {}
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
 
   const moves = history.map((squares, move) => {
     let description;
@@ -97,7 +101,7 @@ function Game() {
       description = "Go to game start";
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
